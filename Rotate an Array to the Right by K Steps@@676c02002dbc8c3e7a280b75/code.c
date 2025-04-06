8691fcb819ac55106f4b6abb5005c2557ccc219d
Shiksha <shiksha251105@gmail.com>
2025-04-06 15:37:59 +0530

    Solution to Rotate an Array to the Right by K Steps

---
+++ Rotate an Array to the Right by K Steps@@676c02002dbc8c3e7a280b75/code.c
@@ -0,0 +1,20 @@
+// Your code here...
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    int k;
+    scanf("%d",&k);
+    int q = k%n;
+    for (int i =0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i = 0;i<q;i++){
+        printf("%d ",arr[i]);
+    }
+    for(int i =q;i<n;i++){
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
